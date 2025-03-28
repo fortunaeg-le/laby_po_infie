d6283faf3e72aa169db1f84dff62381bc35ff731
Егор Лысаков <egor_2705@mail.ru>
2025-03-28 21:03:21 +0300

    второе

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -15,27 +15,33 @@ namespace WinFormsApp1
             number = 12 - (42 / 7);
             number += 10;
             number *= 3;
-            number = 71 / 3;
-            int count = 0;
-            count++;
-            count--;
-            string result = "hello";
-            result += " again " + result;
-            result = "the value is: " + count;
-            result = "";
-            bool yesNo = false;
-            bool anotherBool = true;
-            yesNo = !anotherBool;
+            number = 71 / 3;//ïïï
+
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int count = 0;
+            count++;
+            count--;
+            string result = "hello";
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            result += " again " + result;
+            result = "the value is: " + count;
+            result = "";
+            
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool yesNo = false;
+            bool anotherBool = true;
+            yesNo = !anotherBool;
         }
     }
 }
